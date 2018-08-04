5550ae926e9a602e50af3d2093d49b7d18230425
samuelmattos <muka@outlook.com>
2018-08-03 22:10:25 -0300

    Evolucao Test php

---
+++ src/App/tests/Model/EvolucaoTest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Model\Evolucao;
+class EvolucaoTest extends TestCase{
+
+    public function test_if_evolucao_create()
+    {
+        $evolucao = new Evolucao();
+        $this->assertEquals('evolução','evolução');
+    }
+}

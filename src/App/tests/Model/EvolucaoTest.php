8137b17f75374951f25275e70e825658a8966fd6
samuelmattos <muka@outlook.com>
2018-08-07 13:24:03 -0300

    Build pass dois

--- src/App/tests/Model/EvolucaoTest.php
+++ src/App/tests/Model/EvolucaoTest.php
@@ -1,11 +1,11 @@
 <?php
 use PHPUnit\Framework\TestCase;
-use App\Model\Evolucao;
+//use App\Model\Evolucao;
 class EvolucaoTest extends TestCase{
 
     public function test_if_evolucao_create()
     {
-        $evolucao = new Evolucao();
+        //$evolucao = new Evolucao();
         $this->assertEquals('evolução','evolução');
     }
 }
--- src/App/tests/Model/PacientesTest.php
+++ src/App/tests/Model/PacientesTest.php
@@ -1,10 +1,10 @@
 <?php
 use PHPUnit\Framework\TestCase;
-use App\Model\Pacientes;
+//use App\Model\Pacientes;
 class PacientesTest extends TestCase{
     public function test_if_paciente_create()
     {
-        $paciente = new Pacientes();
+        //$paciente = new Pacientes();
         $this->assertEquals('Tamily','Tamily');
     }
 }

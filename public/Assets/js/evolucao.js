f1f1e36a7ff845068d8c5883a313d21ea94d820b
samuel <samuel@infotv.com.br>
2019-06-06 13:45:56 -0300

    Removi o pop up do cadastrar e editar evolução, agora carrega na lista.

--- public/Assets/js/evolucao.js
+++ public/Assets/js/evolucao.js
@@ -11,10 +11,11 @@ let nova_evolucao = document.querySelector('#nova_evolucao');
 nova_evolucao.onclick = function () {
     var id_paciente = $("#id_paciente").val();
     $("#nova_evolucao").addClass("loading");
-    axios.get('cadastrar/' + id_paciente + '/' + null).then((response) => {       
-        evolucoes_vue.formulario = false;
+    $("#form_cadastro").empty();
+    evolucoes_vue.formulario = false;
+    axios.get('cadastrar/' + id_paciente + '/' + null).then((response) => {
         $("#nova_evolucao").removeClass("loading");
-        $("#form_cadastro").append(response.data)
+        $("#form_cadastro").append(response.data);
     }).catch(error => {
         evolucoes_vue.formulario = true;
         $("#nova_evolucao").removeClass("loading");
@@ -26,15 +27,12 @@ function cadastrarEvolucao() {
     var formData = getFormData($("#cad_evolucao"));
     $("#cad_evolucao").addClass("loading");
     axios.post('salvar', formData).then((response) => {
+        evolucoes_vue.formulario = true;
         $("#cad_evolucao").removeClass("loading");
-        $('.ui.modal').modal('hide');
-        $(".ui.modal").remove();
         var avaliacao = response.data.evolucao;
         evolucoes_vue.evolucoes.push(avaliacao);
     }).catch(error => {
         $("#cad_evolucao").removeClass("loading");
-        $('.ui.modal').modal('hide');
-        $(".ui.modal").remove();
         popError('Erro', error.response.data.error.message);
     });
 };
@@ -49,8 +47,7 @@ function editarEvolucao(id_evolucao, index) {
                 evol[key] = value;
             });
             evolucoes_vue.evolucoes.splice(index, 1, evol)
-            $('.ui.modal').modal('hide');
-            $(".ui.modal").remove();
+            evolucoes_vue.formulario = true;
         }
         $("#cad_evolucao").removeClass("loading");
     }).catch(error => {
@@ -77,8 +74,9 @@ function removeEvolucao(id, id_paciente, index) {
 
 function viewEditEvolucao(id_evolucao, index) {
     $("#cad_evolucao").addClass("loading");
+    evolucoes_vue.formulario = false;
     axios.get('viewEdit/' + id_evolucao + '/' + index).then((response) => {
-        popCadastro(response.data);
+        $("#form_cadastro").append(response.data);
         $("#cad_evolucao").removeClass("loading");
     });
 };

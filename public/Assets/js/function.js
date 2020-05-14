55c737919145bd85a3037b757db6fddd299e6336
Samuel Mattos <muka@outlook.com>
2020-05-13 22:11:53 -0300

    Modal buton callback

--- public/Assets/js/function.js
+++ public/Assets/js/function.js
@@ -1,71 +1,70 @@
 function getFormData($form) {
-    var unindexed_array = $form.serializeArray();
-    var formdata = new FormData();
-    $.map(unindexed_array, function (n, i) {
-        formdata.append(n['name'], n['value']);
-    });
-    return formdata;
+  var unindexed_array = $form.serializeArray();
+  var formdata = new FormData();
+  $.map(unindexed_array, function (n, i) {
+    formdata.append(n['name'], n['value']);
+  });
+  return formdata;
 }
 
 function popCadastro(body) {
-    $(".modal").remove();
-    $("body").append(body);
-    $('.modal').modal('show');
+  $(".modal").remove();
+  $("body").append(body);
+  $('.modal').modal('show');
 }
 
 function popError(title, message) {
-    $(".modal").remove();
-    var modal_b = '<div class="modal fade" tabindex="-1" role="dialog" id="myModal">'+
-    '<div class="modal-dialog" role="document">'+
-      '<div class="modal-content">'+
-        '<div class="modal-header">'+
-          '<h5 class="modal-title">'+title+'</h5>'+
-          '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
-            '<span aria-hidden="true">&times;</span>'+
-          '</button>'+
-       '</div>'+
-        '<div class="modal-body">'+
-        message +
-        '</div>'+
-        '<div class="modal-footer">'+
-          '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'+
-        '</div>'+
-      '</div>'+
-   '</div>'+
-  '</div>';
-    $("body").append(modal_b);
-    $('#myModal').modal('show');
+  $(".modal").remove();
+  var modal_b = '<div class="modal fade" tabindex="-1" role="dialog" id="myModal">' +
+    '<div class="modal-dialog" role="document">' +
+    '<div class="modal-content">' +
+    '<div class="modal-header">' +
+    '<h5 class="modal-title">' + title + '</h5>' +
+    '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+    '<span aria-hidden="true">&times;</span>' +
+    '</button>' +
+    '</div>' +
+    '<div class="modal-body">' +
+    message +
+    '</div>' +
+    '<div class="modal-footer">' +
+    '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+  $("body").append(modal_b);
+  $('#myModal').modal('show');
 }
 
 function creat_popup(title, message, callback) {
   $(".modal").remove();
-  var modal_b = '<div class="modal fade" tabindex="-1" role="dialog" id="myModal">'+
-  '<div class="modal-dialog" role="document">'+
-    '<div class="modal-content">'+
-      '<div class="modal-header">'+
-        '<h5 class="modal-title">'+title+'</h5>'+
-        '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
-          '<span aria-hidden="true">&times;</span>'+
-        '</button>'+
-     '</div>'+
-      '<div class="modal-body">'+
-      message +
-      '</div>'+
-      '<div class="modal-footer">'+
-        '<button type="button" class="btn btn-default" id="modal-btn-si">Sim</button>'+
-        '<button type="button" class="btn btn-primary" id="modal-btn-no">Não</button>'+
-      '</div>'+
-    '</div>'+
- '</div>'+
-'</div>';
+  var modal_b = '<div class="modal fade" tabindex="-1" role="dialog" id="myModal">' +
+    '<div class="modal-dialog" role="document">' +
+    '<div class="modal-content">' +
+    '<div class="modal-header">' +
+    '<h5 class="modal-title">' + title + '</h5>' +
+    '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+    '<span aria-hidden="true">&times;</span>' +
+    '</button>' +
+    '</div>' +
+    '<div class="modal-body">' +
+    message +
+    '</div>' +
+    '<div class="modal-footer">' +
+    '<button type="button" class="btn btn-default" id="modal-btn-si">Sim</button>' +
+    '<button type="button" class="btn btn-primary" id="modal-btn-no">Não</button>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
   $("body").append(modal_b);
   $('.modal').modal('show');
-  $("#modal-btn-si").on("click", function(){
+  $("#modal-btn-si").on("click", function () {
     callback(true);
     $(".modal").modal('hide');
   });
-  $("#modal-btn-no").on("click", function(){
-    callback(false);
+  $("#modal-btn-no").on("click", function () {    
     $(".modal").modal('hide');
   });
 }

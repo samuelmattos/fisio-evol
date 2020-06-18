456dda3c06afa2ab8e80a1fe6b836a37324665bf
Samuel Mattos <muka@outlook.com>
2020-06-17 21:26:35 -0300

    Add debug mode with whoops packpage

--- src/App/Core/Middlewares.php
+++ src/App/Core/Middlewares.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Core;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Config;
+
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 class Middlewares 
 {
@@ -25,5 +26,34 @@ class Middlewares
     public function user()
     {
         return new AdminMiddleware($this->responseFactory, $this->config->login['user']);      
+    }
+
+    public function api()
+    {
+        return new \Tuupola\Middleware\JwtAuthentication([
+            "path" => ["/api"],
+            "regexp" => "/(.*)/",
+            "header" => "X-Token",
+            "ignore" => [
+                "/api/auth/login"
+            ],
+            "error" => function ($response, $arguments) {
+                $data["status"] = "error";
+                $data["message"] = $arguments["message"];
+                return $response
+                    ->withHeader("Content-Type", "application/json")
+                    ->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+            },
+            "secret" => 'xxxxxx'
+        ]);
+    }
+
+    public function error()
+    {
+        return new \Zeuxisoo\Whoops\Slim\WhoopsMiddleware([
+            'enable' => Config::DEBUG_APP,
+            'editor' => 'vscode',
+            'title'  => 'Ops algo deu errado',        
+        ]);
     }
 }

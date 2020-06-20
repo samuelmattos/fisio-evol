2efcc513ed582739c825e9f14124b7ff9160e3c6
Samuel Mattos <muka@outlook.com>
2020-06-20 15:59:40 -0300

    I need change the extension vue to js Because the delimtier don't work in .vue

--- src/App/js/main.js
+++ src/App/js/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import RegisterEvolution from './components/evolution.js'
 import axios from 'axios'
 import 'bootstrap'
 import $ from 'jquery'
 import 'jquery-mask-plugin';
 window.axios = axios;
 window.$ = window.jQuery = $;
+
+Vue.component('evolution', RegisterEvolution)
 window.Vue = Vue;
